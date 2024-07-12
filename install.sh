d163ce7e804c87c213e302f43cae9a1a67bff082
王昊 <13921265345@163.com>
2024-07-12 15:36:53 -0400

    add readme

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ pip uninstall -y spatha
 rm -rf build
 rm -rf dist
 rm -rf spatha.egg-info
-# python3 -W ignore setup.py build
-# python3 -W ignore setup.py install
+python3 -W ignore setup.py build
+python3 -W ignore setup.py install
